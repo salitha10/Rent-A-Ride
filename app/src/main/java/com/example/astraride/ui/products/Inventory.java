928919c4a6ad9b408f432beba8d04d2d123b9bc1
salitha10 <salithaniranjana@gmail.com>
2021-09-23 02:17:41 +0530

    recyclerview not working

--- app/src/main/java/com/example/astraride/ui/products/Inventory.java
+++ app/src/main/java/com/example/astraride/ui/products/Inventory.java
@@ -2,14 +2,26 @@ package com.example.astraride.ui.products;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.astraride.R;
+import com.example.astraride.models.Item;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,6 +34,10 @@ public class Inventory extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    DatabaseReference dbf;
+    Item item;
+    ArrayList<Item> itemList;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -55,6 +71,7 @@ public class Inventory extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
 
@@ -62,13 +79,62 @@ public class Inventory extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-        MyListAdapter adapter = new MyListAdapter(myListData);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(adapter);
 
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inventory, container, false);
+        //Get data from database
+        dbf = FirebaseDatabase.getInstance().getReference().child("Items");
+        itemList = new ArrayList<>();
+
+        dbf.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.hasChildren()) {
+
+                    //Retrieve all data
+                    for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+
+                        Item item = new Item();
+                        item.setItemID(dataSnapshot.child("itemID").getValue().toString());
+                        item.setBrand(dataSnapshot.child("brand").getValue().toString());
+                        item.setCategory(dataSnapshot.child("category").getValue().toString());
+                        item.setColor(dataSnapshot.child("color").getValue().toString());
+                        item.setCapacity(dataSnapshot.child("capacity").getValue().toString());
+                        item.setDatePosted(dataSnapshot.child("datePosted").getValue().toString());
+                        item.setDetails(dataSnapshot.child("details").getValue().toString());
+                        item.setItemImage(dataSnapshot.child("itemImage").getValue().toString());
+                        item.setLocation(dataSnapshot.child("location").getValue().toString());
+                        item.setRentalFee(dataSnapshot.child("rentalFee").getValue().toString());
+                        item.setTitle(dataSnapshot.child("title").getValue().toString());
+
+                        itemList.add(item);
+                        Log.d("title", item.getTitle());
+
+                    }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e("DBError", error.getMessage());
+            }
+        });
+
+
+        View view = inflater.inflate(R.layout.fragment_inventory, container, false);
+
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recycler);
+        ItemRecyclerViewAdapter adapter = new ItemRecyclerViewAdapter(itemList);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/example/astraride/ui/products/ItemRecyclerViewAdapter.java
+++ app/src/main/java/com/example/astraride/ui/products/ItemRecyclerViewAdapter.java
@@ -1,21 +1,35 @@
 package com.example.astraride.ui.products;
 
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.astraride.R;
+import com.example.astraride.models.Item;
+
+import java.util.ArrayList;
 
 public class ItemRecyclerViewAdapter extends RecyclerView.Adapter<ItemRecyclerViewAdapter.ViewHolder>{
+
+    ArrayList<Item> itemList = new ArrayList<>();
+
+    public ItemRecyclerViewAdapter(ArrayList<Item> itemList) {
+        this.itemList = itemList;
+    }
+
     @NonNull
     @Override
-    public ItemRecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
         View listItem= layoutInflater.inflate(R.layout.inventory_item_card_view, parent, false);
         ViewHolder viewHolder = new ViewHolder(listItem);
@@ -23,25 +37,36 @@ public class ItemRecyclerViewAdapter extends RecyclerView.Adapter<ItemRecyclerVi
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ItemRecyclerViewAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+//        final ArrayList myListData = listdata[position];
+//        holder.itemImage.setImageURI(Uri.parse(itemList.get(position).getItemImage()));
+        holder.itemtxt.setText("Hello");
 
-
+//        holder.relativeLayout.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(view.getContext(),"click on item: "+ position,Toast.LENGTH_LONG).show();
+//            }
+//        });
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return itemList.size();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        public ImageView imageView;
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView itemImage;
         public TextView textView;
         public RelativeLayout relativeLayout;
+        public LinearLayout linearLayout;
+        public TextView title, brand, category, capacity, location, fee, details, color, itemtxt;
+
         public ViewHolder(View itemView) {
             super(itemView);
-            this.imageView = (ImageView) itemView.findViewById(R.id.itemImg);
-            this.textView = (TextView) itemView.findViewById(R.id.textView);
-            relativeLayout = (RelativeLayout)itemView.findViewById(R.id.relativeLayout);
+            itemImage=itemView.findViewById(R.id.itemImg);
+            itemtxt=itemView.findViewById(R.id.itemName);
+            linearLayout=itemView.findViewById(R.id.layout_id);
         }
     }
 }
